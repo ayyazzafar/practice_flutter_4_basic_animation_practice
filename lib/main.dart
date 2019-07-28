d5c13a01d50fb163b44385492a828ee98d3be69f
Ayyaz Zafar (Ayyaz) <ayyaz.zaffar@gmail.com>
2019-07-28 22:21:38 +0500

    animation 2

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,48 @@ class LogoApp extends StatefulWidget {
   _LogoAppState createState() => _LogoAppState();
 }
 
-class _LogoAppState extends State<LogoApp> {
+class _LogoAppState extends State<LogoApp> with TickerProviderStateMixin{
+
+  Animation<double> animation;
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(vsync: this, duration: const Duration(seconds:2));
+    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    controller.forward();
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 10),
-        height: 300,
-        width: 300,
-        child: FlutterLogo(),
-      ),
+      child: AnimatedLogo(animation: animation),
     );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+}
+
+class AnimatedLogo extends AnimatedWidget{
+  AnimatedLogo({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+
+      @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+    return Container(
+        margin: EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width: animation.value,
+        child: FlutterLogo(),
+      );
   }
 }
